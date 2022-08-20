5e89597faa65a8ec700b4e6bdb9094e8289311a9
RosanaNei <rosageene@gmail.com>
2022-08-20 13:17:07 -0300

    want to

--- js/portada.js
+++ js/portada.js
@@ -1,15 +1,27 @@
 function changeImage() {
 	var Image_Id = document.getElementById('imagen1');
+	let titulo = document.querySelector(".mono");
+	let anchor = document.querySelector(".anchor-mono");
+	let anchor2 = document.querySelector(".anchor-mono2");
 	
+
+	titulo.classList.toggle("satu")
+	anchor.classList.toggle("anchor-satu")
+	anchor2.classList.toggle("anchor-satu2")
 	if (Image_Id.src.match("imgs/monocroma.jpg")) {
 		Image_Id.src = "imgs/saturada.jpg";
 		document.body.style.backgroundColor = "rgb(158, 25, 25)";
-		document.getElementsByTagName("h1").style.textColor="yellow";
+		
+		
 	}
 	else {
 		Image_Id.src = "imgs/monocroma.jpg";
 		document.body.style.backgroundColor = "#161414";
-		document.querySelector("a").style.color="lightgray;";
+		
 		
 	}
 	};
+	
+	
+        
+	
