71227084e4e29a7f0d35d89fa88062f33b704be6
RosanaNei <rosageene@gmail.com>
2022-08-20 17:27:23 -0300

    yes

--- js/apaisadas.js
+++ js/apaisadas.js
@@ -5,10 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
 		outDuration: 100
 	});
 });
-
-function toggle() {
+/*
+function toggle(){
 	var x = document.getElementsById("nota");
 	if (x.style.display == "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
+function mostrar() {
+	div = document.getElementById('flotante');
+	div.style.display = '';
+}
+
+function cerrar() {
+	div = document.getElementById('flotante');
+	div.style.display = 'none';
+}*/
+function myFunction() {
+	var x = document.getElementById("myDIV");
+	if (x.style.display === "none") {
 	  x.style.display = "block";
 	} else {
 	  x.style.display = "none";
